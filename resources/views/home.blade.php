7f477526c817d60ced7744a109eb7a07242aa85b
Dewi Maya Fitriana <maya.dmf@gmail.com>
2015-08-07 09:08:32 +0700

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,13 @@
                     <div class="panel-body">
                         You are logged in!
                     </div>
+                    <div class="panel-body">
+                        <marquee>
+                            <font color="blue" size="6">
+                                Selamat Datang di Gudang ATK ... ^_^
+                            </font>
+                        </marquee>
+                    </div>
                 @else
                     <div class="panel-body">
                         Please login!
