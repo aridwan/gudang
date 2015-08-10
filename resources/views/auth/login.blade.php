db9c0d6535034b8d26ce8504d0c7b1c5b0d1e36b
ridwan <aridwan05@gmail.com>
2015-08-10 15:33:10 +0700

    login pake nama

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,7 +28,7 @@
                         <div class="form-group">
                             <label class="col-md-4 control-label">Name</label>
                             <div class="col-md-6">
-                                <input type="text" class="form-control" name="email" value="{{ old('name') }}">
+                                <input type="text" class="form-control" name="name" value="{{ old('name') }}">
                             </div>
                         </div>
                         <div class="form-group">
