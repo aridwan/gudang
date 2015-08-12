698e45b17f80132c0ae680b71df90c9f538e6119
Dewi Maya Fitriana <maya.dmf@gmail.com>
2015-08-12 13:33:15 +0700

    seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,10 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
+        $this->call('UserTableSeeder'::class);
+        $this->call('AnggotaTableSeeder'::class);
+        
+
         // $this->call(UserTableSeeder::class);
 
         Model::reguard();
